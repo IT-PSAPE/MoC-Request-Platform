445078948a862350614325f787516ca4070861d3
Craig Chihururu <craigchihururu@gmail.com>
2025-10-02 06:52:39 +0200

    Set Next.js output mode to 'export'
    Added the 'output: export' option to next.config.ts to enable static export of the Next.js application.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   env: {
     SUPABASE_URL: 'https://jrsrekqsmdziwfhusevb.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impyc3Jla3FzbWR6aXdmaHVzZXZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3MzEyNDksImV4cCI6MjA3MjMwNzI0OX0.UNIC7eYl2IVOXI-QsVPAsYXAicX3vp6HyJdCYa_cwjc'
