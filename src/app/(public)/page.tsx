8d99fc392c30504fbdac03ec3bdf874444ccfe86
Craig Chihururu <craigchihururu@gmail.com>
2025-11-07 19:10:23 +0200

    refactor: remove unused Image import from public page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image";
 import Card from "@/components/ui/Card";
 import Button from "@/components/ui/Button";
 import Text from "@/components/ui/text";
